0e24ba74e8168dc9e76155841ddb95e0a48cfd9d
Rokurolize <1701388+Rokurolize@users.noreply.github.com>
2026-06-18 23:31:52 +0900

    [codex] Add renderer fixture coverage (#3)
    * Add renderer fixture coverage
    
    * Use block containers for footnote contents
    
    * Use block wrapper for footnote tooltips

--- src/render/html/element/footnotes.rs
+++ src/render/html/element/footnotes.rs
@@ -54,7 +54,7 @@ pub fn render_footnote(ctx: &mut HtmlContext) {
             // tooltip and link to work. A screen reader can still navigate
             // through to the link and read the footnote directly.
             ctx.html()
-                .span()
+                .div()
                 .attr(attr!(
                     "class" => "wj-footnote-ref-tooltip",
                     "aria-hidden" => "true",
@@ -68,7 +68,7 @@ pub fn render_footnote(ctx: &mut HtmlContext) {
 
                     // Actual tooltip contents
                     ctx.html()
-                        .span()
+                        .div()
                         .attr(attr!("class" => "wj-footnote-ref-contents"))
                         .contents(contents);
                 });
@@ -136,7 +136,7 @@ pub fn render_footnote_block(ctx: &mut HtmlContext, title: Option<&str>) {
 
                             // Footnote contents
                             ctx.html()
-                                .span()
+                                .div()
                                 .attr(attr!("class" => "wj-footnote-list-item-contents"))
                                 .contents(contents);
                         });
